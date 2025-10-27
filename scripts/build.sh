719f0c1b60ed0a4c3b19191c3b7f3349ef815898
Fredrik Frodlund <fredrik@frodlund.se>
2025-10-27 21:48:59 +0100

    ci: remove all dokploy references

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# Load and export environment variables from dokploy.env
-set -a  # automatically export all variables
-source ./env/dokploy.env
-set +a  # stop automatically exporting
-
 # Set build-time environment variables
 export PUBLIC_BUILD_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export PUBLIC_VERSION="$(git rev-parse --short HEAD)"
