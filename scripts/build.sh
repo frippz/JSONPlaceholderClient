3eeeaed1bdfcbb2365ede84098424745f2c21392
Fredrik Frodlund <fredrik@frodlund.se>
2025-10-27 21:12:57 +0100

    ci: things

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,4 @@ export PUBLIC_BUILD_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export PUBLIC_VERSION="$(git rev-parse --short HEAD)"
 
 # Run the build
-pnpm build
+pnpm run build
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -7,9 +7,9 @@ import {
   PUBLIC_SITE_FOLDER,
 } from '$env/static/public';
 
-export const version = PUBLIC_VERSION;
-export const buildTime = PUBLIC_BUILD_TIME;
-export const siteTitle = PUBLIC_SITE_TITLE;
-export const baseUrl = PUBLIC_BASE_URL;
-export const siteFolder = PUBLIC_SITE_FOLDER;
-export const apiBaseUrl = PUBLIC_API_BASE_URL;
+export const version = PUBLIC_VERSION || '';
+export const buildTime = PUBLIC_BUILD_TIME || '';
+export const siteTitle = PUBLIC_SITE_TITLE || '';
+export const baseUrl = PUBLIC_BASE_URL || '';
+export const siteFolder = PUBLIC_SITE_FOLDER || '';
+export const apiBaseUrl = PUBLIC_API_BASE_URL || '';
