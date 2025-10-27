5b4716fd9f9590d3ca524ad550e1a5dfbf7d2f6e
Fredrik Frodlund <fredrik@frodlund.se>
2025-10-27 21:39:16 +0100

    ci: build thingie

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Set build-time environment variables
+source ./env/dokploy.env
 export PUBLIC_BUILD_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export PUBLIC_VERSION="$(git rev-parse --short HEAD)"
 
