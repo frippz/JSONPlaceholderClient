699a032284d6621be156b4ea09f392723a9a181f
Fredrik Frodlund <fredrik@frodlund.se>
2025-11-22 18:28:04 +0100

    feat(users): add API calls and types for users endpoint (#279)

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
 <h2>Users</h2>
 <p>Coming soon…</p>
+<pre>{JSON.stringify(data, null, 2)}</pre>
